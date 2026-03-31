45aec8ab225f9efd1673107155ccc4e5865f9b60
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-03-31 17:25:33 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BookiaApp());
+}
+class BookiaApp extends StatelessWidget {
+  const BookiaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
