bed105d7ffd6af9f4e2c6f67c6bfed0ab4b8e378
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-03-31 17:40:20 +0200

    feat: add core helper functions (navigation and validation)

---
+++ lib/core/functions/navigations.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void pushReplacement(BuildContext context, Widget newScreen) {
+  Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(builder: (context) => newScreen),
+  );
+}
+
+void pushTo(BuildContext context, Widget newScreen) {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => newScreen));
+}
+
+void pushToBase(BuildContext context, Widget newScreen) {
+  Navigator.pushAndRemoveUntil(
+    context,
+    MaterialPageRoute(builder: (context) => newScreen),
+    (route) => false,
+  );
+}
+
+void pop(BuildContext context) {
+  Navigator.pop(context);
+}
