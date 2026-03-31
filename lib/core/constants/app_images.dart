ac01ba1559a39728ae24f08652a7c6fcb557d66a
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-03-31 17:39:46 +0200

    feat: add app constants (images and fonts)

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,16 @@
+abstract class AppImages {
+  static const String logoSvg = 'assets/images/logo.svg';
+  static const String successSvg = 'assets/images/success.svg';
+  static const String bg = 'assets/images/bg.png';
+  static const String appleSvg = 'assets/icons/apple.svg';
+  static const String backSvg = 'assets/icons/back.svg';
+  static const String bookmarkSvg = 'assets/icons/bookmark.svg';
+  static const String cameraSvg = 'assets/icons/camera.svg';
+  static const String cartSvg = 'assets/icons/cart.svg';
+  static const String googleSvg = 'assets/icons/google.svg';
+  static const String homeSvg = 'assets/icons/home.svg';
+  static const String logoutSvg = 'assets/icons/logout.svg';
+  static const String profileSvg = 'assets/icons/profile.svg';
+  static const String searchSvg = 'assets/icons/search-normal.svg';
+  static const String loadingJson = 'assets/images/loading.json';
+}
