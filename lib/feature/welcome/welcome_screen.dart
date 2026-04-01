cb5074f86147a16dc240dfe9a2bef29cbd1ff2b4
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-04-01 15:55:19 +0200

    feat: add Welcome screen layout

---
+++ lib/feature/welcome/welcome_screen.dart
@@ -0,0 +1,59 @@
+import 'package:bookia/core/constants/app_images.dart';
+import 'package:bookia/core/styles/colors.dart';
+import 'package:bookia/core/styles/text_styles.dart';
+import 'package:bookia/core/widgets/custom_svg_picture.dart';
+import 'package:bookia/core/widgets/main_button.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            AppImages.bg,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Positioned(
+            top: 0,
+            left: 20,
+            right: 20,
+            bottom: 0,
+            child: Column(
+              children: [
+                Spacer(flex: 2),
+                CustomSvgPicture(path: AppImages.logoSvg, width: 250),
+                Gap(30),
+                Text('Order Your Book Now!', style: TextStyles.subtitle1),
+                Spacer(flex: 5),
+                MainButton(
+                  text: 'Login',
+                  onPressed: () {
+                    // pushTo(context, LoginScreen());
+                  },
+                ),
+                Gap(15),
+                MainButton(
+                  borderColor: AppColors.darkColor,
+                  bgColor: AppColors.backgroundColor,
+                  textColor: AppColors.darkColor,
+                  text: 'Register',
+                  onPressed: () {
+                    // pushTo(context, RegisterScreen());
+                  },
+                ),
+                Spacer(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'dart:io';
 import 'package:bookia/core/styles/themes.dart';
 import 'package:bookia/feature/splash/splash_screen.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     DevicePreview(
-      enabled: !kReleaseMode,
+      // enabled: !kReleaseMode,
+      enabled: false,
       builder: (context) => const BookiaApp(),
     ),
   );
