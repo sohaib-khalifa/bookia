dc51911faaec6832288c4ec99c689fd708376279
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-04-01 15:50:03 +0200

    feat: add device_preview dependency and implement SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,40 @@
+import 'dart:io';
+import 'package:bookia/core/styles/themes.dart';
+import 'package:bookia/feature/splash/splash_screen.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const BookiaApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const BookiaApp(),
+    ),
+  );
 }
+
 class BookiaApp extends StatelessWidget {
   const BookiaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppThemes.lightTheme,
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(
+            context,
+          ).copyWith(textScaler: TextScaler.linear(1.0)),
+          child: SafeArea(
+            top: false,
+            bottom: Platform.isAndroid,
+            child: child!,
+          ),
+        );
+      },
+      home: SplashScreen(),
+    );
   }
 }
