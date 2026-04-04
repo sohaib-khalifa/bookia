54f21f4157c99012aca301818084330e8602f015
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-04-04 11:08:36 +0200

    feat(auth): connect login screen with AuthCubit and form handling
    Integrated AuthCubit with LoginScreen using BlocListener
    Handled auth states (loading, success, error) with dialogs and navigation
    Connected form fields with cubit controllers and validation
    Triggered login API call on form submission

--- lib/feature/auth/presentation/login_register/screens/login_screen.dart
+++ lib/feature/auth/presentation/login_register/screens/login_screen.dart
@@ -1,17 +1,24 @@
+// import 'dart:developer';
+
 import 'package:bookia/core/constants/app_images.dart';
+import 'package:bookia/core/functions/validations.dart';
 import 'package:bookia/core/routes/navigations.dart';
 import 'package:bookia/core/routes/routes.dart';
 import 'package:bookia/core/styles/colors.dart';
 import 'package:bookia/core/styles/text_styles.dart';
 import 'package:bookia/core/widgets/custom_svg_picture.dart';
 import 'package:bookia/core/widgets/custom_text_form_field.dart';
+import 'package:bookia/core/widgets/dialogs.dart';
 import 'package:bookia/core/widgets/main_button.dart';
 import 'package:bookia/core/widgets/my_body_view.dart';
 import 'package:bookia/core/widgets/password_text_form_field.dart';
+import 'package:bookia/feature/auth/presentation/cubit/auth_cubit.dart';
+import 'package:bookia/feature/auth/presentation/cubit/auth_state.dart';
 import 'package:bookia/feature/auth/presentation/login_register/widgets/social_login.dart';
 import 'package:bookia/feature/auth/presentation/widgets/auth_footer.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -19,20 +26,51 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Row(
-          children: [
-            GestureDetector(
-              onTap: () => pop(context),
-              child: CustomSvgPicture(path: AppImages.backSvg),
-            ),
-          ],
+    return BlocListener<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthSuccessState) {
+          pop(context);
+          // log("success");
+          pushToBase(context, Routes.home);
+        } else if (state is AuthErrorState) {
+          pop(context);
+          showErrorDialog(context, state.message);
+        } else if (state is AuthLoadingState) {
+          showLoadingDialog(context);
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Row(
+            children: [
+              GestureDetector(
+                onTap: () => pop(context),
+                child: CustomSvgPicture(path: AppImages.backSvg),
+              ),
+            ],
+          ),
+        ),
+        body: _loginBody(context),
+        bottomNavigationBar: AuthFooter(
+          label: 'Don\'t have an account?',
+          buttonLabel: 'Sign Up',
+          onTap: () {
+            pushReplacement(context, Routes.register);
+          },
         ),
       ),
-      body: MyBodyView(
-        child: SingleChildScrollView(
+    );
+  }
+
+  MyBodyView _loginBody(BuildContext context) {
+    var cubit = context.read<AuthCubit>();
+
+    return MyBodyView(
+      child: SingleChildScrollView(
+        child: Form(
+          autovalidateMode: AutovalidateMode.onUnfocus,
+          key: cubit.formKey,
           child: Column(
             children: [
               Text(
@@ -40,19 +78,36 @@ class LoginScreen extends StatelessWidget {
                 style: TextStyles.headline,
               ),
               Gap(32),
-              CustomTextFormField(hintText: 'Enter your email'),
+              CustomTextFormField(
+                controller: cubit.emailController,
+                hintText: 'Enter your email',
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your email';
+                  } else if (!isEmailValid(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+              ),
               Gap(16),
-              PasswordTextFormField(hintText: 'Enter your password'),
+              PasswordTextFormField(
+                controller: cubit.passwordController,
+                hintText: 'Enter your password',
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
               Gap(10),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
                     onPressed: () {
-                      pushTo(
-                        context,
-                        Routes.forgotPassword,
-                      );
+                      pushTo(context, Routes.forgotPassword);
                     },
                     child: Text(
                       'Forgot Password?',
@@ -64,19 +119,19 @@ class LoginScreen extends StatelessWidget {
                 ],
               ),
               Gap(30),
-              MainButton(text: 'Login', onPressed: () {}),
+              MainButton(
+                text: 'Login',
+                onPressed: () {
+                  if (cubit.formKey.currentState!.validate()) {
+                    cubit.login();
+                  }
+                },
+              ),
               Gap(30),
               SocialLogin(),
             ],
           ),
         ),
-      ),
-      bottomNavigationBar: AuthFooter(
-        label: 'Don\'t have an account?',
-        buttonLabel: 'Sign Up',
-        onTap: () {
-          pushReplacement(context, Routes.register);
-        },
       ),
     );
   }
